f3efb4990addcb583d00fc8faf66846fb0782202
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-06 17:29:43 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const appendedCats = [...cats, name]
+    return appendedCats
+}
+
+function prependCat(name) {
+    const prependedCats = [name, ...cats]
+    return prependedCats
+}
+
+function removeLastCat() {
+    const removedLastCat = cats.slice(0, -1)
+    return removedLastCat
+}
+
+function removeFirstCat() {
+    const removedFirstCat = cats.slice(1)
+    return removedFirstCat
+}
